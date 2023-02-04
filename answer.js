0135411d032061ee960f98448458295a455b07ea
jamie-lars <benjaminlarbie30@gmail.com>
2023-02-04 21:56:59 +0000

    3rd-js

---
+++ answer.js
@@ -0,0 +1,28 @@
+//Question 1
+var arr = [[1, 2, 3],[4, 5, 6],7,8,9,[[10, 11, 12, [13, 4, 112]], 136, 14],];
+console.log(arr[5][0][0]);
+console.log(arr[3]);
+console.log(arr[5][0][3][0]);
+console.log(arr[5][1]);
+console.log(arr[5][0][3][1]);
+
+//Question 2
+var sentence = ["javascript", "web", "Capacity Bay"];
+console.log("I am a " + sentence[1] + " developer at " + sentence[2] + " and I love " + sentence[0]);
+
+
+//Question 3
+let word = "Generalization";
+console.log(word.slice(3, -1));
+
+//Question 4
+var fruits = ["orange", "apple", "guava", "lemon"];
+
+// Adding elements to the beginning of the array
+fruits.unshift("pea", "banana");
+
+// Adding elements to the end of the array
+fruits.push("Avocado", "mango");
+
+console.log(fruits);
+
